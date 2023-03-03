0c1587f58f65583d9d5e2371aa75ba4100b44619
junwensoh <junwen.soh@thecarousell.com>
2023-03-03 15:40:03 +0800

    Template for processing webhook data

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -2,7 +2,28 @@ import { Request, Response } from "express";
 
 async function createNewProduct(req: Request, res: Response) {
   try {
-    res.send({ message: "product created!!" });
+    if (!req.body) {
+      const msg = "no Pub/Sub message received";
+      console.error(`error: ${msg}`);
+      res.status(400).send(`Bad Request: ${msg}`);
+      return;
+    }
+    if (!req.body.message) {
+      const msg = "invalid Pub/Sub message format";
+      console.error(`error: ${msg}`);
+      res.status(400).send(`Bad Request: ${msg}`);
+      return;
+    }
+
+    const pubSubMessage = req.body.message;
+    
+    const name = pubSubMessage.data
+      ? Buffer.from(pubSubMessage.data, "base64").toString().trim()
+      : "World";
+
+    console.log(Buffer.from(pubSubMessage.data, "base64"));
+    console.log(`Hello ${name}!`);
+    res.status(204).send();
   } catch (error) {
     console.log(error);
     res.status(500).send();
