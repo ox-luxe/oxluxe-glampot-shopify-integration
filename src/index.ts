2b59bfeb7894f9cb2d14e15bde23c87b58099ad1
junwensoh <junwen.soh@thecarousell.com>
2023-03-03 16:24:54 +0800

    Add parser

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import { routes } from "./routes";
 
 const app: Express = express();
-
+app.use(express.json());
 app.use("/", routes());
 
 app.listen(process.env.PORT || 8080, () => {
