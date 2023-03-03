e3622e8b870d0d60ca4a581f29fa9db4c31f0092
junwensoh <junwen.soh@thecarousell.com>
2023-03-03 17:24:16 +0800

    Create seperation between development and production for pubsub message data

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -2,28 +2,18 @@ import { Request, Response } from "express";
 
 async function createNewProduct(req: Request, res: Response) {
   try {
-    if (!req.body) {
-      const msg = "no Pub/Sub message received";
-      console.error(`error: ${msg}`);
-      res.status(400).send(`Bad Request: ${msg}`);
-      return;
-    }
-    if (!req.body.message) {
-      const msg = "invalid Pub/Sub message format";
-      console.error(`error: ${msg}`);
-      res.status(400).send(`Bad Request: ${msg}`);
-      return;
-    }
-
-    const pubSubMessage = req.body.message;
-    console.log(pubSubMessage);
-    console.log(Buffer.from(pubSubMessage.data, "base64"));
     
-    const name = pubSubMessage.data
-      ? Buffer.from(pubSubMessage.data, "base64").toString().trim()
-      : "World";
+    const pubSubMessage = req.body.message;
+    let productData;
 
-    console.log(`Hello ${name}!`);
+    if (process.env.NODE_ENV === "development") {
+      productData = pubSubMessage.data;
+    }
+
+    if (process.env.NODE_ENV === "production") {
+      productData = Buffer.from(pubSubMessage.data, "base64").toJSON().data;
+    }
+    console.log(productData);
     res.status(204).send();
   } catch (error) {
     console.log(error);
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { verifyPubsubMessage } from "../middleware/verifyPubsubMessage";
 import { productControllers } from "../controllers/productControllers";
 const router = express.Router();
 
 function productRoute() {
-  router.route("/glampot").post(productControllers.createNewProduct);
+  router
+    .route("/glampot")
+    .post(verifyPubsubMessage, productControllers.createNewProduct);
   return router;
 }
 
