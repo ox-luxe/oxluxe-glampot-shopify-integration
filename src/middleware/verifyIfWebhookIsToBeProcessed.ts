a1f36c7ed213933530b095c1201e1ef5f624f467
junwensoh <junwen.soh@thecarousell.com>
2023-03-07 14:18:23 +0800

    Abstract webhook processing verification into it's own middleware

---
+++ src/middleware/verifyIfWebhookIsToBeProcessed.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { ShopifyStore } from "../services/ShopifyStore";
+
+export async function verifyIfWebhookIsToBeProcessed(req: Request, res: Response, next: NextFunction) {
+  try {
+    const pubSubMessage = req.body.message;
+    let productWebhook;
+
+    if (process.env.NODE_ENV === "development") {
+      productWebhook = pubSubMessage.data;
+    }
+
+    if (process.env.NODE_ENV === "production") {
+      productWebhook = JSON.parse(
+        Buffer.from(pubSubMessage.data, "base64").toString()
+      );
+    }
+
+    const shouldSyncProductToGlampot = ShopifyStore.doesProductCreateWebhookContainTag(productWebhook, "Glampot");
+    if (shouldSyncProductToGlampot) {
+      return next();
+    }
+    res.status(204).send();
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+}
+
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyPubsubMessage } from "../middleware/verifyPubsubMessage";
 import { productControllers } from "../controllers/productControllers";
+import { verifyIfWebhookIsToBeProcessed } from "../middleware/verifyIfWebhookIsToBeProcessed";
 const router = express.Router();
 
 function productRoute() {
+
+  router.use(verifyIfWebhookIsToBeProcessed);
+  
   router
     .route("/glampot")
     .post(verifyPubsubMessage, productControllers.createNewProduct);
