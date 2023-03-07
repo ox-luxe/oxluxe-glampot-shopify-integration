4682ecaf60b7df75d5c4f779600f071cc0e9eceb
junwensoh <junwen.soh@thecarousell.com>
2023-03-07 14:29:59 +0800

    Create product update endpoint to process product/update webhooks

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -7,10 +7,17 @@ const router = express.Router();
 function productRoute() {
 
   router.use(verifyIfWebhookIsToBeProcessed);
-  
+
   router
     .route("/glampot")
     .post(verifyPubsubMessage, productControllers.createNewProduct);
+
+  router
+  .route("/glampot/update")
+  .post((req, res) => {
+    console.log(req.body);
+    res.status(204).send();
+  })
   return router;
 }
 
