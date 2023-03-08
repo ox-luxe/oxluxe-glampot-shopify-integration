5487f1d4a3f059262e38d0ef699546af72322677
junwensoh <junwen.soh@thecarousell.com>
2023-03-08 17:02:58 +0800

    Setup handler in product delete endpoint and test webhook

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { verifyPubsubMessage } from "../middleware/verifyPubsubMessage";
 import { verifyWebhookType } from "../middleware/verifyWebhookType";
 import { productControllers } from "../controllers/productControllers";
+import { OneToOneProductMapping } from "../models/OneToOneProductMapping";
+import { ShopifyStore } from "../services/ShopifyStore"; 
 import { extractProductWebhookForFurtherProcessing } from "../middleware/extractProductWebhookForFurtherProcessing";
 const router = express.Router();
 
@@ -9,12 +11,12 @@ function productRoute() {
   router.use(
     extractProductWebhookForFurtherProcessing, // ends here if product is not tagged with "Glampot"
     verifyPubsubMessage, // ends here if webhook does not have expected shape
-    verifyWebhookType // assigns webhook to be either processed by createNewProduct / updateProduct controller
   );
 
   router
     .route("/")
     .post(
+      verifyWebhookType,
       productControllers.createNewProduct,
       productControllers.updateProduct,
       productControllers.deleteProduct,
@@ -22,8 +24,19 @@ function productRoute() {
 
   router
     .route("/delete")
-    .post((req, res) => {
+    .post(async (req, res) => {
+      let productWebhook = res.locals.productWebhook;
+      console.log(productWebhook.id);
       console.log('DELETING PRODUCT');
+      // all we have in the productWebhook is the id
+
+      // if the product is deleted, we will not be able to find the product listing anymore hence no tag info
+
+      // we will then proceed to find if there is an associated glampot product id in the DB
+
+      // if there is, delete the corresponding glampot product
+
+      // if there isn't, do nothing.
       res.status(204).send();
     })
 
